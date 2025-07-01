06b6d011d1c48d6e70014a4c34bd6b1c10a035f1
Aashi Singh <aashiishere.2004@gmail.com>
2025-07-02 02:36:49 +0530

    schema

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app=express();
 
-const path = require("path");
-app.use("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
+const mongoose = require('mongoose');
 const Chat= require("./models/chats.js");
 
-const mongoose = require('mongoose');
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+
 
 main().then(() => {
   console.log("Connected to MongoDB");
@@ -16,9 +18,18 @@ main().then(() => {
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/CHAT_APP');
-}
+};
 
+let chat1= new Chat({
+    from: "Alice",
+    to: "Bob",
+    message: "Hello Bob!",
+    created_at: new Date(), //UTC date
+});
 
+chat1.save().then((res) => {
+    console.log(res)
+});
 
 
 app.get("/", (req, res) => {
