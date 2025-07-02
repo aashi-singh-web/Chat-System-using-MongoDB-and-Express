8ee06ebe2294f5545c41c6b3cb358d1681f9d102
Aashi Singh <aashiishere.2004@gmail.com>
2025-07-03 01:45:53 +0530

    Edit Route Created

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({ extended: true }));
 
 
 main().then(() => {
@@ -32,6 +33,32 @@ app.get("/chats", async (req,res)=>{
 //new route
 app.get("/chats/new", (req, res) => {
   res.render("new.ejs");
+});
+
+//create route
+app.post("/chats", (req, res) => {
+  let{ from,to,message}=req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    message: message,
+    created_at: new Date()
+  });
+
+  newChat.save().then(() => {
+    console.log("New chat saved successfully");
+  }).catch((err) => {
+    console.log("Error saving new chat:", err);
+});
+  res.redirect("/chats");
+});
+
+
+//edit route
+app.get("/chats/:id/edit", async (req, res) => {
+  let {id} = req.params;
+  let chat = await Chat.findById(req.params.id);
+  res.render("edit.ejs", { chat });
 });
 
 
